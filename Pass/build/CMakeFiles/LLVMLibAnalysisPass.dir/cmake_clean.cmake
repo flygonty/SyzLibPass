7ec0ae3d21effa203e4a5c95413626e4010d87dd
flygonty <willy.liu4610@gmail.com>
2023-08-14 00:42:30 +0800

    Upload the skeleton

---
+++ Pass/build/CMakeFiles/LLVMLibAnalysisPass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLVMLibAnalysisPass.dir/LibAnalysis.cpp.o"
+  "CMakeFiles/LLVMLibAnalysisPass.dir/LibAnalysis.cpp.o.d"
+  "libLLVMLibAnalysisPass.pdb"
+  "libLLVMLibAnalysisPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLVMLibAnalysisPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Pass/build/CMakeFiles/LibAnalysisPass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LibAnalysisPass.dir/LibAnalysis.cpp.o"
+  "CMakeFiles/LibAnalysisPass.dir/LibAnalysis.cpp.o.d"
+  "libLibAnalysisPass.pdb"
+  "libLibAnalysisPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LibAnalysisPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
